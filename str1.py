19f9eab7e914e94a28a6c15e712b3a8718d8bf95
Benjamin Imo Uka <benjaminukaimo@gmail.com>
2025-06-06 13:30:32 +0100

    Update str1.py

--- str1.py
+++ str1.py
@@ -375,8 +375,8 @@ def create_plot(data, price_line_colors, rsi_10_colors, symbol, interval, market
     visible_indicators = ["RSI", "Fisher", "Bias", "Divergence"]
     num_plots = 1 + len(visible_indicators)
 
-    width = 50  # Very wide
-    height = 22 * num_plots  # Tall
+    width = 45  # Very wide
+    height = 17 * num_plots  # Tall
 
     fig = Figure(figsize=(width, height), dpi=70)
     gs = fig.add_gridspec(num_plots, 1, height_ratios=[17] + [7]*len(visible_indicators))
