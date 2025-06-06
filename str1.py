53e59753a259f9fd18e1aa5a5076e0b1487542f5
Benjamin Imo Uka <benjaminukaimo@gmail.com>
2025-06-06 04:38:56 +0100

    Update str1.py

--- str1.py
+++ str1.py
@@ -387,7 +387,7 @@ def create_plot(data, price_line_colors, rsi_10_colors, symbol, interval, market
 
     for ax in axes:
         ax.tick_params(axis='both', which='major',
-                      labelsize=42, width=4, length=8, pad=8)
+                      labelsize=42, width=7, length=8, pad=10)
         ax.tick_params(axis='both', which='major', labelsize=42)
 
         for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
